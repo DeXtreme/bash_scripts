0751ca77b90eada930a5972c729a1dfb63011c2a
Imperius <dbakolbire@gmail.com>
2023-06-06 19:48:07 +0100

    Refactor listdir.sh Colours the text of the default and given path in the output

--- listdir.sh
+++ listdir.sh
@@ -11,14 +11,14 @@ list_contents(){
 
 if [ $# -eq 1 ]; then  # Check if an argument was given
 	if [ -d $1 ]; then  #Check if argument is valid path
-		echo "Listing files and folders in $1"
+		echo "Listing files and folders in \e[1;34m$1\e[m"
 		list_contents $1
 	else
 		echo "Invalid path"
 		echo "Usage : listdir.sh [path/to/directory]"
 	fi
 elif [ $# -eq 0 ]; then # Check if no arguemnt was given
-	echo "Listing files and folders in /usr/local"
+	echo "Listing files and folders in \e[1;34m/usr/local\e[m"
 	list_contents /usr/local
 else
 	echo "Usage : listdir.sh [path/to/directory]"
